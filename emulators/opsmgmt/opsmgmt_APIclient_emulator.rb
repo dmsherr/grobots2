d85c53f87161d927143d0afdb7819c6ed5bc6d44
iryanb <32988577+iryanbinary@users.noreply.github.com>
2018-08-13 09:18:11 -0700

    Add files via upload
    more deliverables

---
+++ emulators/opsmgmt/opsmgmt_APIclient_emulator.rb
@@ -0,0 +1,41 @@
+# Author: Ryan Bagnulo @iryanb 
+# SCA Lift Status Operational Management API client application emulator
+# Copyright 2018 All Rights Reserved. guarden.io 
+# Usage License: MIT
+
+require 'rest-client'
+#require "net/http"
+require 'json'
+require 'optparse'
+require 'time'
+
+gettime = Time.now
+
+puts gettime.to_s
+
+n=0      
+# increase the below number to simulate more concurrent user requests                                                      
+5.times do                                                          
+
+responseMsg = RestClient.get "http://p0st3r.gateway.akana.com/SCA/v0/IntelligentAgent/lifts", 'Accept'=>"application/json"
+responseMsgparsed = JSON.parse(responseMsg)
+fieldA = responseMsgparsed["LiftVibrationHz"]
+#fieldB = responseMsgparsed[0]["FieldB"]
+#fieldC = responseMsgparsed[0]["FieldC"]
+
+n=n+1
+
+#parsed
+#puts n.to_s + " Response Message "  + fieldA + " " + fieldB + " " + fieldC 
+
+puts n.to_s + " Response Message " + responseMsgparsed.to_s 
+puts "************** PARSED JSON FIELD ****************"
+puts n.to_s + " LiftVibrationHz = " + fieldA.to_s 
+
+end                                                                  
+
+elapsed = Time.now - gettime
+
+puts "Completed " + n.to_s + "api requests " + " in " + elapsed.to_s + " seconds." 
+
+puts "The Time now is " + Time.now.to_s 
